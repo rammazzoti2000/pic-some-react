2347c2f7c084499819f21fbd7ea219d0a63e1085
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-11-14 15:09:17 +0100

    Fetch data and save it to State using useEffect hook

--- src/context/CreateContextProvider.js
+++ src/context/CreateContextProvider.js
@@ -1,12 +1,21 @@
 /* eslint-disable */
 
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import propTypes from 'prop-types';
 
 const AppContext = createContext();
 
 function CreateContextProvider({ children }) {
   const [photos, setPhotos] = useState([]);
+  const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
+
+  useEffect(() => {
+    fetch(url)
+      .then(resp => resp.json())
+      .then(json => setPhotos(json))
+  }, [])
+
+  console.log(photos)
 
   return (
     <AppContext.Provider value={{ photos }}>
